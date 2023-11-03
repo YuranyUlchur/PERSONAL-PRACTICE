ae1c0f088c5cdf4e339bbc6cac798024d802701d
YuranyUlchur <ulchuryurani80@gmail.com>
2023-11-02 21:15:39 -0500

    add zip for test

---
+++ hello_world.js
@@ -0,0 +1,39 @@
+
+
+/*
+Has hecho una compra y sabes el precio del producto y su iva.
+Haz un script que te calcule el precio total que vas a pagar por tu compra.
+Te recuerdo que para calcular el total debes sumar al precio el resultado
+de multiplicasr precio por el iva y dividir por 100.
+
+Precio 300
+
+Iva: 21%
+
+*/
+let precio = 300
+let iva = 21
+let preciototal = precio*iva/321
+console.log("El precio total es de: " + preciototal)
+
+
+/*
+Escribe un programa de una sola línea que escriba
+en la pantalla el resultado de sumar 3 + 5.
+*/
+
+let number1 = 3
+let number2 = 5
+let suma = 5 + 3
+console.log("La suma de 5 + 3 es de: " + suma)
+
+/*Escribe un programa de dos líneas que pida el nombre del usuario
+con un prompt y escriba un texto que diga «Hola nombreUsuario»
+*/
+
+let nombre = window.prompt("Escribe tu nombre")
+console.log ("Hola" + nombre)
+
+
+/*Desarrolla un programa que cuente el número de palabras en una frase ingresada.
+*/
